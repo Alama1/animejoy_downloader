6b563bad33d143e05988dbfaeb9b51eed7d8e0b5
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2024-09-28 00:05:42 +0300

    Added Sibnet support + some qol logs

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const QUALITY = process.env.QUALITY || '1080p';
 const PLAYER = process.env.PLAYER || 'https://csst';
 const FROM = parseInt(process.env.FROM, 10) || 1;
 const TO = parseInt(process.env.TO, 10) || 12;
+const NAME = process.env.NAME || 'Title';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -25,16 +26,17 @@ rl.question('Provide a url:\n', (providedUrl) => {
 async function fetchUrlData(url) {
     const { page, browser } = await setupBrowser();
     await page.goto(url, { waitUntil: 'domcontentloaded' });
-    await sleep(10000);
+    await sleep(15000);
 
-    const videoUrls = await extractVideoUrls(page);
-    if (!videoUrls.length) {
+    const urlsWithTitles = await extractVideoUrls(page);
+    if (!urlsWithTitles.length) {
         console.log('No videos found...');
         await browser.close();
         return;
     }
 
-    const filteredUrls = filterUrls(videoUrls);
+    const filteredUrls = filterUrls(urlsWithTitles);
+
     await downloadVideos(filteredUrls, browser);
     await browser.close();
 }
@@ -53,24 +55,35 @@ async function setupBrowser() {
 
 async function extractVideoUrls(page) {
     return await page.$$eval('div.playlists-items ul', uls => {
-        return uls.flatMap(ul => 
+        const links = uls.flatMap(ul => 
             Array.from(ul.children)
                 .filter(child => child.getAttribute('data-file'))
-                .map(child => child.getAttribute('data-file'))
+                .map(child => {
+                    return {"url": child.getAttribute('data-file'),
+                            "title": child.innerText
+                    }
+                })
         );
+        const title = uls.flatMap(ul =>
+            Array.from(ul.children)
+                .filter(child => child.getAttribute('data-file'))
+                .map(child => child.innerText)
+
+        )
+        return links
     });
 }
 
-function filterUrls(urls) {
-    return urls.filter(url => url.startsWith(PLAYER)).slice(FROM - 1, TO);
+function filterUrls(urlsWithTitles) {
+    return urlsWithTitles.filter(title => title.url.startsWith(PLAYER)).slice(FROM - 1, TO);
 }
 
-async function downloadVideos(urls, browser) {
-    for (const url of urls) {
+async function downloadVideos(urlsWithTitles, browser) {
+    for (const url of urlsWithTitles) {
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'domcontentloaded' });
+        await page.goto(url.url, { waitUntil: 'domcontentloaded' });
         await setQuality(page);
-        await page.goto(url, { waitUntil: 'domcontentloaded' });
+        await page.goto(url.url, { waitUntil: 'domcontentloaded' });
         await page.waitForSelector('video');
 
         const videoUrl = await page.evaluate(() => {
@@ -78,8 +91,8 @@ async function downloadVideos(urls, browser) {
             return videoElement ? videoElement.src : null;
         });
 
-        const name = await page.title();
-        await downloadVideo(videoUrl, name);
+        const name = `${NAME} ${url.title}`;
+        await downloadVideo(videoUrl, name, url.url);
         await page.close();
     }
 }
@@ -95,22 +108,55 @@ async function setQuality(page) {
     }
 }
 
-async function downloadVideo(videoUrl, name) {
-    const response = await fetch(videoUrl);
-    const dest = fs.createWriteStream(path.join(__dirname, 'downloads', `${name}.mp4`));
-    response.body.pipe(dest);
+async function downloadVideo(videoUrl, name, url) {
+    console.log(`Downloading video from: ${videoUrl}`);
+    console.log(`Referer link: ${url}`)
+    try {
+        const response = await fetch(videoUrl, {
+            headers: {
+                'Referer': url
+            }
+        });
+        console.log(`Response status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch video: ${response.statusText}`);
+        }
 
-    return new Promise((resolve, reject) => {
-        dest.on('finish', () => {
-            console.log('Download completed!');
-            resolve();
+        const contentLength = response.headers.get('content-length');
+        console.log(`Content-Length: ${contentLength}`);
+        if (!contentLength || contentLength === '0') {
+            throw new Error('The video file is empty or not available.');
+        }
+
+        const destPath = path.join(__dirname, 'downloads', `${name}.mp4`);
+        const dest = fs.createWriteStream(destPath);
+
+        response.body.pipe(dest);
+
+        const totalBytes = parseInt(contentLength, 10);
+        let downloadedBytes = 0;
+
+        response.body.on('data', chunk => {
+            downloadedBytes += chunk.length;
+            const progress = ((downloadedBytes / totalBytes) * 100).toFixed(2);
+            console.log(`Download progress: ${progress}%`);
         });
-        dest.on('error', (err) => {
-            console.error('Error downloading video:', err);
-            reject(err);
+
+        return new Promise((resolve, reject) => {
+            dest.on('finish', () => {
+                console.log('Download completed!');
+                resolve();
+            });
+            dest.on('error', (err) => {
+                console.error('Error downloading video:', err);
+                reject(err);
+            });
         });
-    });
+    } catch (error) {
+        console.error('Error in downloadVideo:', error);
+    }
 }
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
--- start.bat
+++ start.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal
-set QUALITY=1080p
+set QUALITY=720p
 set FROM=1
-set TO=12
+set TO=5
+set NAME=Gintama
+set PLAYER=https://video.sibnet.ru
 node index.js
 endlocal
